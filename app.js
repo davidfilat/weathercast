6e0dfa2649348fa7e44b362e2ce62b834ad55589
David Filat <david.filat@eurasiaprecept.org>
2021-04-05 18:58:36 +0300

    hide OPEN WeatherMap API key

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 import axios from 'axios';
-
+import { OPEN_WEATHER_MAP_API_KEY } from './credentials.js';
 const city = process.argv[2];
 
 async function printCurrentWeather(cityName) {
   const OPEN_WEATHER_MAP_API =
-    'https://api.openweathermap.org/data/2.5/weather?q=' +
-    cityName +
-    '&appid=***REMOVED***&units=metric&lang=ro';
+    `https://api.openweathermap.org/data/2.5/weather?q=${cityName}` +
+    `&appid=${OPEN_WEATHER_MAP_API_KEY}&units=metric&lang=ro`;
 
   try {
     const response = await axios.get(OPEN_WEATHER_MAP_API);
