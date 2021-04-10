13142abc0be53742b8839362c5a6dd47706e764f
David Filat <david.filat@eurasiaprecept.org>
2021-04-10 23:59:25 +0300

    solves issue with unescaped characters in city name

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 import { printCurrentWeather, printWeatherFor7Days } from './weatherAPI.js';
-const city = process.argv[2];
+const city = encodeURI(process.argv[2]);
 
 const coords = await printCurrentWeather(city);
 printWeatherFor7Days(coords);
