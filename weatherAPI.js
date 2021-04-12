715cbae62c04a6a6e0c453d213a04cdc492365dd
David Filat <david.filat@eurasiaprecept.org>
2021-04-12 19:49:52 +0300

    adds error handling to getData()

--- weatherAPI.js
+++ weatherAPI.js
@@ -11,9 +11,27 @@ async function getData(url) {
   try {
     const response = await axios.get(url);
     const data = response.data;
+    console.log(data);
     return data;
   } catch (error) {
-    console.log(error.message);
+    const errorMessages = {
+      404:
+        'Denumirea orașului nu este validă. ' +
+        'Vă rugăm verificați dacă ați introdus corect numele orașului.',
+      401: 'API key este incorectă. Vă rugăm verificați fișierul credentials.js.',
+      429: 'Ați depășit limita de cererei către OpenWeatherMap API.',
+      500: 'Ne pare rău, a apărut o eroare internă a serverului.',
+      ENOTFOUND:
+        'Nu există o conexiune cu internetul.' +
+        'Verificați setările și aparatajul pentru interent.',
+      get EAI_AGAIN() {
+        return this.ENOTFOUND;
+      },
+    };
+
+    const errorCode = error.code || Number(error.response.data.cod);
+    console.log(errorMessages[errorCode]);
+    process.exit();
   }
 }
 
