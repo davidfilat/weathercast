b93a30aa87224b591e0a48c76aefd7c43fad921c
David Filat <david.filat@eurasiaprecept.org>
2021-04-12 17:10:56 +0300

    adds main function to app.js
    Signed-off-by: David Filat <david.filat@eurasiaprecept.org>

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 import { printCurrentWeather, printWeatherFor7Days } from './weatherAPI.js';
 const city = encodeURI(process.argv[2]);
 
-const coords = await printCurrentWeather(city);
-printWeatherFor7Days(coords);
+async function main() {
+  const coords = await printCurrentWeather(city);
+  printWeatherFor7Days(coords);
+}
+
+main();
